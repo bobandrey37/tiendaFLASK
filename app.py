9fddd9265a1a8ab73b1465fa28fd7fe7b1288bdc
_wxrhd <pmespert@gmail.com>
2020-04-12 00:18:28 +0200

    Falta todo el apartado de compras, lo demas bien

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_mysqldb import MySQL
 #hashed pw
 from werkzeug.security import generate_password_hash, check_password_hash
 
+#initializations
 app = Flask(__name__)
 
 #MySQL database connection 
@@ -16,48 +17,48 @@ app.config['MYSQL_DB'] = 'tienda'
 mysql = MySQL(app)
 
 #settings 
-app.secret_key = "mysecretkey" 
+app.secret_key = 'mysecretkey'
 
 #routing
-@app.route("/", methods=["GET","POST"])
+@app.route('/', methods=['GET','POST'])
 def index():
-    return "Welcome to our shop!"
+    return 'Welcome to our shop!'
 
-
-@app.route("/signup", methods=["GET","POST"])
+@app.route('/signup', methods=['GET','POST'])
 def signup():
     
-    if request.method == "POST":
-        user = request.form["user"]
-        hashed_pw = generate_password_hash(request.form["password"], method="sha256")
-        email = request.form["email"]
+    if request.method == 'POST':
+            user = request.form["user"]
+            hashed_pw = generate_password_hash(request.form["password"], method="sha256")
+            email = request.form["email"]
 
-        #curl
-        curl = mysql.connection.cursor()
-        curl.execute("INSERT into users (username, password, email) VALUES (%s,%s,%s)",
-        (user, hashed_pw,email))
+            #curl
+            curl = mysql.connection.cursor()
+            curl.execute("INSERT INTO users (username, password, email) VALUES (%s,%s,%s)",
+            (user, hashed_pw,email))
 
-        #commit
-        mysql.connection.commit()
+            #commit
+            mysql.connection.commit()
 
-        #mensaje entre vistas
-        flash("Genial! te has registrado.", "success")
+            #mensaje entre vistas
+            flash('Flawless! you have just been registered','success')
 
-        return redirect(url_for("login"))
+            return redirect(url_for('login'))
 
-    return render_template("signup.html")
+    return render_template('signup.html')
 
-@app.route("/login", methods=["GET","POST"])
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
-@app.route("/search",methods=["GET","POST"])
+@app.route('/search',methods=['GET','POST'])
 def search():
-    my_username = request.form["user"]
+    my_username = request.form['user']
     
     #curl
     curl = mysql.connection.cursor()
-    curl.execute("SELECT * from users where username=(%s)",(my_username,))
+    curl.execute('SELECT * from users where username=(%s)',(my_username,))
 
     #commit
     mysql.connection.commit()
@@ -69,67 +70,30 @@ def search():
 
     objeto_usuario = data
 
-    if objeto_usuario and check_password_hash(objeto_usuario[0][2],request.form["password"]):
+    if objeto_usuario and check_password_hash(objeto_usuario[0][2],request.form['password']):
         #creamos la sesion
-        session["username"] = objeto_usuario[0][1]
+        session['username'] = objeto_usuario[0][1]
 
+        return render_template("index.html",user=objeto_usuario[0][1])
 
-        return render_template("index.html", user=data[0])
+    return redirect(url_for('login'))
 
-    return redirect(url_for("login"))
-
-@app.route("/logout", methods=["GET","POST"])
+@app.route('/logout', methods=["GET","POST"])
 def logout():
-    session.pop("username",None) #pasamos none para evitar errores
 
-    return "You are logged out"
+    session.pop('username',None) #pasamos none para evitar errores
 
-@app.route("/add_products",methods=["GET","POST"])
-def add_products():
-    return render_template("add_products.html")
+    return redirect(url_for('index'))
 
+@app.route('/home')
+def home():
 
-@app.route("/shopping", methods=["GET","POST"])
-def adding():
-    if(request.method == "POST"):
+    if 'username' in session:
 
-        product = request.form["product"]
-        units = request.form["units"]
-        color = request.form["color"] 
+        return render_template('index.html')
 
-        #curl
+    flash('You must log in first to access shopping page!','error')
+    return redirect(url_for('login'))
 
-        curl = mysql.connection.cursor()
-        curl.execute("INSERT INTO products (product, units, color) VALUES (%s, %s, %s)",
-        (product, units, color))
-
-        #commit
-        mysql.connection.commit()
-
-        curl.close()
-
-        #mensaje entre vistas
-        flash("product added succesfully", "success")
-
-        return render_template("index.html")
-
-@app.route("/cart", methods=["GET", "POST"])
-def cart():
-
-        #curl
-        curl = mysql.connection.cursor()
-        curl.execute("SELECT * from products")
-
-        #commit
-        mysql.connection.commit()
-
-        data = curl.fetchall()
-
-        curl.close()
-
-        flash("mostrando carrito.. ", "success")
-
-        return render_template("add_products.html", products=data[0])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
